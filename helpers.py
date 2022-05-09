ad171d6de04d470880d57d3d8885c623272e8b10
Israjur Rahman <irahman@idare.io>
2022-05-09 17:34:39 +0600

    random object methods initiated

---
+++ helpers.py
@@ -0,0 +1,25 @@
+import random
+
+
+# alphabetical strings
+def get_random_alphabetical_strings():
+    """Generates random alphabetical strings."""
+    pass
+
+
+# real numbers
+def get_random_real_numbers():
+    """Generates random real numbers."""
+    pass
+
+
+# integers
+def get_random_integers():
+    """Generates random integers."""
+    pass
+
+
+# alphanumerics
+def get_random_alphanumerics():
+    """Generates random alphanumerics."""
+    pass
