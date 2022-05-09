8d1196d3d31dd680dcd3db8fa425f3257968c68f
Israjur Rahman <irahman@idare.io>
2022-05-09 17:34:51 +0600

    max file size check added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -6,49 +7,74 @@ from starlette.responses import FileResponse
 
 from pathlib import Path
 
+from helpers import (
+    get_random_integers,
+    get_random_alphanumerics,
+    get_random_alphabetical_strings,
+    get_random_real_numbers
+)
+
 app = FastAPI(title="Random Objects")
 
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-RECIPES = {"tea": "milk", "rice": "water"}
 OUTPUT_FILE_NAME = "output.txt"
-MAXSIZE = 1024
+MAXFILESIZE = 2000000  # max file size in bytes
+
+RANDOM_OBJECT_METHODS = {
+    "alphabetical": get_random_alphabetical_strings,
+    "real": get_random_real_numbers,
+    "integers": get_random_integers,
+    "alphanumerics": get_random_alphanumerics
+}
+
 
 @app.get("/")
 def root(request: Request) -> dict:
+    """Renders main page when application starts"""
     return TEMPLATES.TemplateResponse(
         "main.html",
         {"request": request},
     )
-    
+
 
 @app.get("/generate")
 def generate(request: Request) -> dict:
+    """Generates output file with random objects and returns download link"""
     generate_random_objects()
     return TEMPLATES.TemplateResponse(
         "main.html",
         {
-            "request": request, 
+            "request": request,
             "download_url": f"{request.url_for('root')}download/{OUTPUT_FILE_NAME}"
         },
     )
 
-    
+
 def generate_random_objects():
-    open (OUTPUT_FILE_NAME, 'w')
-    file_size = os.stat(OUTPUT_FILE_NAME).st_size
-    print(file_size)
-    with open(OUTPUT_FILE_NAME, 'a') as f:                # max file size in bytes
-        while file_size >- MAXSIZE:
-            f.write('file contents\n')
+    """Generates a output file with random object strings"""
+    # clearing output file
+    open(OUTPUT_FILE_NAME, 'w').close()
+    # setting initial file size to 0
+    file_size = 0
+    # writing contents to file
+    with open(OUTPUT_FILE_NAME, 'a') as f:
+        while file_size < MAXFILESIZE:
+            random_type = random.choice(list(RANDOM_OBJECT_METHODS.keys()))
+            output = RANDOM_OBJECT_METHODS[random_type]
+            f.write(output + ', ')
+            f.tell()
+
+            # updating current file size
+            file_size = os.stat(OUTPUT_FILE_NAME).st_size
 
 
 @app.get("/download/{item}")
 def download_file(request: Request, item: str):
+    """Downloads file using requested item in url"""
     return FileResponse(
-        f"{BASE_PATH}/{item}", 
+        f"{BASE_PATH}/{item}",
         media_type='application/octet-stream',
         filename=OUTPUT_FILE_NAME
-        )
-
+    )
